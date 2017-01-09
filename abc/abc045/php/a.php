4ab385747750df7ebef8f28a7e8a0c3685e3aee5
TONY0922 <galepilot@gmail.com>
2017-01-09 21:57:13 +0900

    modify abc045

---
+++ abc/abc045/php/a.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+  http://abc045.contest.atcoder.jp/tasks/abc045_a
+*/
+
+$stdins = [];
+while(true) {
+  $stdin = trim(fgets(STDIN));
+  if ($stdin === '') {
+    break;
+  }
+  $stdins[] = $stdin;
+}
+
+
+ ?>
---
+++ abc/abc045/php/b.php
@@ -0,0 +1,7 @@
+<?php
+
+/*
+  http://abc045.contest.atcoder.jp/tasks/abc045_b
+*/
+
+ ?>
